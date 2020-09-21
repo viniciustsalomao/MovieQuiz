9b7c4cf5319ab9bd1d8cab946c4fa91c11a1bc03
Vinícius Tinajero Salomão <viniciustsalomao@hotmail.com>
2020-09-21 19:37:49 -0300

    Initial Commit

---
+++ MovieQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieQuiz
+//
+//  Created by Vinícius Tinajero Salomão on 21/09/20.
+//  Copyright © 2020 Vinícius Tinajero Salomão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
